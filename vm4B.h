242a8b08d2943df98fe1d7f9d5c1bce336816940
TheSchmiddle <schmitty403@hotamail.com>
2015-10-07 03:44:25 +0000

    Made fileIO.c

---
+++ vm4B.h
@@ -0,0 +1,154 @@
+/*
+* Nibble Knowlege
+* ------------------------------------------------------
+* FileName: vm4B.h.c
+* Description: Contains the definitions and constants used thoughout
+*	       the VM
+* Original Creator: Bailey Tye
+* Last Editor:      Bailey Tye
+* Date of Last Edit: 03/10/15
+* Date of Creation:  01/10/15
+*/
+
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <inttypes.h>
+
+#define VERSTR "VM4 v0.1"
+#define IOMEM 3
+#define BOOTMEM 1024
+#define RESERVEMEM (1 + IOMEM + BOOTMEM)
+#define NOERROR 0
+#define MEMALLOCERROR 1
+#define UNKOWNERROR -1
+#define MEMADDRSIZE 16
+#define MEMSIZE 65536
+#define BIGEND -1
+#define LITTLEEND 1
+#define MEMMODSIZE 4
+
+#define PLATEND LITTLEEND
+
+#define NUMREG 4
+#define PC 0
+#define A 1
+#define MEM 2
+#define STAT 3
+
+#define HLT 0
+#define LOD 1
+#define STR 2
+#define ADD 3
+#define NOP 4
+#define NND 5
+#define JMP 6
+#define CXA 7
+
+
+#define REGISTER16BIT 0
+#define REGISTER4BIT 1
+
+
+// Structure of a nibble
+
+typedef struct _nibble{
+	uint8_t data : 4;
+	} nibble;
+
+	// Global Variables
+
+	extern uint16_t regPC;
+	extern nibble   regA;
+	extern uint16_t regMEM;
+	extern nibble	regSTAT;
+
+	extern nibble*	MAINMEM;
+
+	//--------------------Prototypes----------------------//
+
+
+	// inst.c Prototypes
+	// ------------------------------------------------//
+
+
+	//Decodes the OP code and Calls the appropriate instruction
+	int decode(char* op_code, uint16_t address);
+
+	//Halt Instruction
+	void hlt(void);
+
+	//Load Instruction
+	void lod(uint16_t);
+
+	//Store Instruction
+	void str(uint16_t);
+
+	//Add Instruction
+	void add(uint16_t);
+
+	//No Operation Instruction
+	void nop(void);
+
+	//NAND Instruction
+	void nnd(uint16_t);
+
+	//Carry XOR into Accumulator
+	void cxa(void);
+
+	//Jump Instruction
+	void jmp(uint16_t);
+
+	//Prints registers to screen
+	void printReg(void);
+
+	//Set the XOR bit in the stat register
+	void setXOR(void);
+
+	// mem.c Prototypes
+	// ------------------------------------------------//
+
+	//Initialize Memory
+	int initMem(void);
+
+	//Stores data into memory
+	void writeMem(nibble data, uint16_t address);
+
+	//Prints all of Memory
+	void printMem(uint16_t, uint16_t);
+
+	//Frees Memory allocation
+	void freeMem(void);
+
+	//Sets data table values for 1 to 15
+	void setBoot(void);
+
+
+
+	//main.c Prototypes
+	// ------------------------------------------------//
+
+	//Superloop
+	int mainloop(void);
+
+	//Shuts down the virtual machine
+	int shutdown(int);
+
+
+	//util.c Prototypes
+	//-------------------------------------------------//
+
+
+	char *tobitstr(size_t,uint64_t,char);
+
+
+	//fileIO.c Prototypes
+	//------------------------------------------------//
+
+	//Reads file into memory
+	int readBin(char*);
